c397ead1b6e19a9ccadda6797fe0e6fc1d820c2f
Kuba2903 <jakub.adamczyk@microsoft.wsei.edu.pl>
2024-10-28 11:58:49 +0100

    create index view for user

---
+++ WebApp/Controllers/UserController.cs
@@ -0,0 +1,32 @@
+﻿using Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApp.Services.Interfaces;
+
+namespace WebApp.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly MovieReservationSystemContext _context;
+
+        private readonly IMovieManagement _management;
+
+        public UserController(MovieReservationSystemContext context, IMovieManagement management)
+        {
+            _context = context;
+            _management = management;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+
+            var films = _context.ShowTimes.Include(x => x.Movie).
+                ThenInclude(x => x.Genre).Where(x => x.ShowDate.HasValue
+                && x.ShowDate >= DateTime.Today);
+
+
+            return View(films);
+        }
+    }
+}
