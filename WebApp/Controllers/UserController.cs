daae66772b63de55061330c9c0fad503f57e2b51
Kuba2903 <jakub.adamczyk@microsoft.wsei.edu.pl>
2024-10-28 12:19:03 +0100

    add filtering by genre and date

--- WebApp/Controllers/UserController.cs
+++ WebApp/Controllers/UserController.cs
@@ -1,4 +1,5 @@
 ﻿using Data;
+using Data.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using WebApp.Services.Interfaces;
@@ -18,12 +19,23 @@ namespace WebApp.Controllers
         }
 
         [HttpGet]
-        public IActionResult Index()
+        public async Task<IActionResult> Index(int? genre, DateTime? dateFrom, DateTime? dateTo)
         {
 
             var films = _context.ShowTimes.Include(x => x.Movie).
                 ThenInclude(x => x.Genre).Where(x => x.ShowDate.HasValue
                 && x.ShowDate >= DateTime.Today);
+
+            if (genre.HasValue)
+                films = films.Where(x => x.Movie.GenreId == genre);
+
+            if(dateFrom.HasValue)
+                films = films.Where(x => x.ShowDate >= dateFrom);
+
+            if (dateTo.HasValue)
+                films = films.Where(x => x.ShowDate <= dateTo);
+
+            ViewBag.Genres = await _management.GetAll<Genre>();
 
 
             return View(films);
