856b4969aab31305f54a17b9b70bffc7e52bf9b3
Kuba2903 <jakub.adamczyk@microsoft.wsei.edu.pl>
2024-11-12 18:50:46 +0100

    create post method for registering users to seats

--- WebApp/Controllers/UserController.cs
+++ WebApp/Controllers/UserController.cs
@@ -3,6 +3,7 @@ using Data.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using WebApp.Services.Interfaces;
 
@@ -29,7 +30,10 @@ namespace WebApp.Controllers
         {
             var user = await _userManager.GetUserAsync(User);
 
-            return user.Id;
+            if(user != null)
+                return user.Id;
+            else
+                return string.Empty;
         }
 
         [HttpGet]
@@ -58,19 +62,42 @@ namespace WebApp.Controllers
 
         [HttpGet]
 
-        public async Task<IActionResult> AddReservation(int showTimeId, string userId)
+        public async Task<IActionResult> AddReservation(int showTimeId)
         {
             var sectors = await _context.SeatReservations.Where(x => x.ShowTimeId == showTimeId)
-                .Select(x => x.Sector).ToListAsync();
-
+                .Select(x => x.Sector).Distinct().ToListAsync();
+            
             var seats = await _context.SeatReservations.Where(x => x.ShowTimeId == showTimeId)
-                .Select(x => x.Seat).ToListAsync();
+                .Select(x => x.Seat).Distinct().ToListAsync();
+
+            var occupiedSeats = await _context.SeatReservations
+                .Where(x => x.ShowTimeId == showTimeId && x.UserId != null)
+                .GroupBy(x => x.Sector)
+                .ToDictionaryAsync(g => g.Key, g => g.Select(seat => seat.Seat).ToList());
 
             ViewBag.Sectors = sectors;
             ViewBag.Seats = seats;
-            
+            ViewBag.Occup = occupiedSeats;
 
-            return View();
+            return View(new SeatReservation() { ShowTimeId = showTimeId});
+        }
+
+        [HttpPost]
+
+        public async Task<IActionResult> AddReservation(SeatReservation entity, string sector, string seat)
+        {
+            var existingReservation = await _context.SeatReservations
+                .FirstOrDefaultAsync(x => x.ShowTimeId == entity.ShowTimeId && x.Sector == sector && x.Seat == seat);
+
+            if (existingReservation != null)
+            {
+                string? id = await GetUser();
+                existingReservation.UserId = id;
+
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToAction("Index");
         }
     }
 }
